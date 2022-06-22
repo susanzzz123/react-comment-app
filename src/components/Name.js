79809e6fc6c0c1aa7450f515e2f4a97402f4e156
Susan Zhang <67386805+susanzzz123@users.noreply.github.com>
2022-06-21 23:16:31 -0500

    Delete Name.js

--- src/components/Name.js
+++
@@ -1,8 +0,0 @@
-const Name = ({name, setName}) => {
-    return (
-        <input type='text' placeholder='Name:' size={20} 
-            onChange={e => setName(e.target.value)} value={name}/>
-    )
-}   
-
-export default Name
