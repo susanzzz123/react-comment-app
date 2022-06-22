ab55f8ff77fa9000fa77dd4ab1b768c5abb33154
Susan Zhang <67386805+susanzzz123@users.noreply.github.com>
2022-06-21 23:16:38 -0500

    Delete Text.js

--- src/components/Text.js
+++
@@ -1,8 +0,0 @@
-const Text = ({message, setMessage}) => {
-    return (
-        <textarea placeholder='Write a new post...' size={50} 
-            onChange={e => setMessage(e.target.value)} value={message}></textarea>
-    )
-}
-
-export default Text
